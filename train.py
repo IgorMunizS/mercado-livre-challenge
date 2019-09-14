e92737b7b9179fbced402db04f7c13c7106e9795
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-14 12:23:08 -0300

    train only with small title

--- train.py
+++ train.py
@@ -159,10 +159,10 @@ def training(languages, EMBEDDING,train,test,type_model,pre):
 
                 X_train, X_val,  X_train_2, X_val_2, X_train_3, X_val_3, Y_train, Y_val = train_test_split(X_train, X_train_2, X_train_3, Y_train, train_size=0.9, random_state=233)
 
-                train_generator = DataGenerator([X_train_2, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model)
-                val_generator = DataGenerator([X_val_2,X_val_3], Y_val, classes, batch_size=batch_size,mode=type_model)
-                # model.get_layer('embedding_layer').set_weights([embedding_matrix])
-                model.get_layer('small_embedding_layer').set_weights([embedding_matrix])
+                train_generator = DataGenerator([X_train, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model)
+                val_generator = DataGenerator([X_val,X_val_3], Y_val, classes, batch_size=batch_size,mode=type_model)
+                model.get_layer('embedding_layer').set_weights([embedding_matrix])
+                # model.get_layer('small_embedding_layer').set_weights([embedding_matrix])
 
             else:
 
