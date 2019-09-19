3e7740c63ba27afce625de82a54f6b1120ce5fdc
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-19 20:14:13 -0300

    test new model

--- model.py
+++ model.py
@@ -74,8 +74,8 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
     embedding_1 = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=True, name='embedding_layer')(sequence_input)
 
     x = SpatialDropout1D(0.3)(embedding_1)
-    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x)
-    x2 = Bidirectional(CuDNNGRU(128, return_sequences=True))(x1)
+    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
+    x2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x1)
     x3 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x2)
     max_pool1 = GlobalMaxPooling1D()(x1)
     max_pool2 = GlobalMaxPooling1D()(x2)
@@ -95,8 +95,8 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
     # x = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x1)
     # max_pool2 = GlobalMaxPooling1D()(x)
 
-    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x)
-    x2 = Bidirectional(CuDNNGRU(128, return_sequences=True))(x1)
+    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
+    x2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x1)
     x3 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x2)
 
     max_pool4 = GlobalMaxPooling1D()(x1)
@@ -105,14 +105,14 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
 
     features_dense = Dense(64, activation="relu")(features_input)
 
-    concat = concatenate([max_pool1,max_pool2,max_pool3,max_pool4,max_pool5,max_pool6,features_dense])
+    x = concatenate([max_pool1,max_pool2,max_pool3,max_pool4,max_pool5,max_pool6,features_dense])
 
     # x = concatenate([max_pool1, max_pool2,features_dense])
-    x = Dense(128, activation='relu')(concat)
-    x = Dropout(0.1)(x)
-    x = BatchNormalization()(x)
-
-    x = concatenate([concat, x])
+    # x = Dense(128, activation='relu')(concat)
+    # x = Dropout(0.1)(x)
+    # x = BatchNormalization()(x)
+    #
+    # x = concatenate([concat, x])
 
     preds = Dense(n_classes, activation="softmax")(x)
     model = Model(inputs=[sequence_input, small_sequence_input, features_input], outputs=preds)
