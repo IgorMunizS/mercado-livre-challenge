c1bd5e5c6601583fd68894cbc5815bf57517555e
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-22 10:40:56 -0300

    new model

--- model.py
+++ model.py
@@ -76,8 +76,8 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
     embedding_1 = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=True, name='embedding_layer')(sequence_input)
 
     x = SpatialDropout1D(0.1)(embedding_1)
-    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
-    x2 = Bidirectional(CuDNNLSTM(32, return_sequences=True))(x1)
+    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x)
+    x2 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x1)
     x3 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x2)
     # x4 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x1)
 
@@ -112,17 +112,22 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
     # x = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x1)
     # max_pool2 = GlobalMaxPooling1D()(x)
 
-    x1 = Bidirectional(CuDNNGRU(128, return_sequences=True))(x)
-    x2 = Bidirectional(CuDNNGRU(32, return_sequences=True))(x1)
-    x3 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x2)
+    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
+    x2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x1)
 
     avg_pool4 = GlobalAveragePooling1D()(x1)
     avg_pool5 = GlobalAveragePooling1D()(x2)
-    avg_pool6 = GlobalAveragePooling1D()(x3)
+    max_pool6 = GlobalMaxPooling1D()(x2)
+
+    x_concat = concatenate([avg_pool4,avg_pool5,max_pool6])
+    dense_1 = Dense(768, activation='relu')(x_concat)
+    dense_2 = Dense(768, activation='relu')(x_concat)
+
+    x_concat_2 = concatenate([x_concat,dense_1,dense_2])
 
     features_dense = Dense(256, activation="relu")(features_input)
 
-    x = concatenate([max_pool1,max_pool2,max_pool3,avg_pool4,avg_pool5,avg_pool6,features_dense])
+    x = concatenate([max_pool1,max_pool2,max_pool3,avg_pool4,x_concat_2,features_dense])
 
     # x = concatenate([max_pool1, max_pool2,features_dense])
     # x = Dense(128, activation='relu')(concat)
