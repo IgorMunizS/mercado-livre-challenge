1def076368cb019c860196c523536e235b42fac9
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-23 10:59:56 -0300

    add training with less samples

--- train_dl.py
+++ train_dl.py
@@ -54,7 +54,7 @@ def training(languages, EMBEDDING,train,test,type_model,pre):
 
         X_test = test_new["title"]
 
-        max_features = 120000
+        max_features = 20000
         maxlen = 20
         embed_size = 300
         batch_size = 512
