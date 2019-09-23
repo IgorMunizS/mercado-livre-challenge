4466bd975d386bbfc7cb146d66cfc8f137bc5f33
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-23 02:26:15 -0300

    add spatial dropout

--- model.py
+++ model.py
@@ -75,8 +75,8 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
 
     embedding_1 = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=True, name='embedding_layer')(sequence_input)
 
-    # x = SpatialDropout1D(0.1)(embedding_1)
-    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(embedding_1)
+    x = SpatialDropout1D(0.1)(embedding_1)
+    x1 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x)
     x2 = Bidirectional(CuDNNLSTM(256, return_sequences=True))(x1)
     x3 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x2)
     # x4 = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x1)
@@ -106,13 +106,13 @@ def get_three_entrys_model(maxlen, max_features,embed_size,embedding_matrix,n_cl
     # embedding_2 = Embedding(max_features, embed_size, weights=[embedding_matrix], trainable=False,
     #                         name='small_embedding_layer')(small_sequence_input)
     #
-    # x = SpatialDropout1D(0.1)(embedding_1)
+    x = SpatialDropout1D(0.1)(embedding_1)
 
     # x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
     # x = Conv1D(64, kernel_size=2, padding="valid", kernel_initializer="he_uniform")(x1)
     # max_pool2 = GlobalMaxPooling1D()(x)
 
-    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(embedding_1)
+    x1 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x)
     x2 = Bidirectional(CuDNNLSTM(128, return_sequences=True))(x1)
 
     avg_pool4 = GlobalAveragePooling1D()(x1)
