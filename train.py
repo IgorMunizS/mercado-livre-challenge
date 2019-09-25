c29fa639c673c3e36a3afc5aa47b39bf5769ed4d
Muniz Soares, Igor <imunizs@indracompany.com>
2019-09-25 08:24:46 -0300

    add SMOTE oversample

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def training(languages, EMBEDDING,train,test,type_model,pre):
         max_features = 100000
         maxlen = 20
         embed_size = 300
-        batch_size = 4096
+        batch_size = 512
 
         # Generate char embedding without preprocess
         text = (train_new['title'].tolist() + test_new["title"].tolist())
@@ -88,14 +88,14 @@ def training(languages, EMBEDDING,train,test,type_model,pre):
 
                 X_train, X_val, X_train_3, X_val_3, Y_train, Y_val = train_test_split(X_train, X_train_3, Y_train, train_size=0.9, random_state=233)
 
-                train_generator = DataGenerator([X_train, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model,resample=False)
+                train_generator = DataGenerator([X_train, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model,resample=True)
                 val_generator = DataGenerator([X_val, X_val_3], Y_val, classes, batch_size=batch_size,mode=type_model, resample=False)
 
             else:
 
                 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, train_size=0.9, random_state=233)
 
-                train_generator = DataGenerator(X_train, Y_train, classes, batch_size=batch_size, resample=False)
+                train_generator = DataGenerator(X_train, Y_train, classes, batch_size=batch_size, resample=True)
                 val_generator = DataGenerator(X_val, Y_val, classes, batch_size=batch_size,resample=False)
 
             opt = Adam(lr=1e-3)
@@ -170,7 +170,7 @@ def training(languages, EMBEDDING,train,test,type_model,pre):
 
                 X_train, X_val, X_train_3, X_val_3, Y_train, Y_val = train_test_split(X_train, X_train_3, Y_train, train_size=0.9, random_state=233)
 
-                train_generator = DataGenerator([X_train, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model,resample=False)
+                train_generator = DataGenerator([X_train, X_train_3], Y_train, classes, batch_size=batch_size,mode=type_model,resample=True)
                 val_generator = DataGenerator([X_val, X_val_3], Y_val, classes, batch_size=batch_size,mode=type_model, resample=False)
                 model.get_layer('embedding_layer').set_weights([embedding_matrix])
                 # model.get_layer('small_embedding_layer').set_weights([embedding_matrix])
