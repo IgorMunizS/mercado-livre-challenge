a267317d39cd2be02e3d33174a223c52b99b3e9b
Muniz Soares, Igor <imunizs@indracompany.com>
2019-12-11 11:48:07 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mercado Libre Data Challenge - 9th Place Solution
+
+In this competition, Mercado Libre, a e-commerce platform,  
+challegend us  to build a model to classify products products into their categories based on their title. 
+
+This repository has the 9th place solution
--- utils/utils.py
+++ utils/utils.py
@@ -33,14 +33,7 @@ def focal_loss(y_true, y_pred):
     alpha = 0.25
     pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
     pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-
-    epsilon = K.epsilon()
-    # clip to prevent NaN's and Inf's
-    pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
-    pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
-
-    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
-           - K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
 
 def label_smooth_loss(y_true, y_pred):
     loss =  tf.keras.losses.categorical_crossentropy(
